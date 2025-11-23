96e4b17175d5cb1429b8bf2ba909c64690cb5014
Mygaverse <125230987+Mygaverse@users.noreply.github.com>
2025-11-24 03:40:48 +0800

    fix the tailwind install issue and update with any previous updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import HeroSection from '../components/HeroSection';
 import FeatureSection from '../components/FeatureSection';
 import AboutSection from '../components/AboutSection';
 import CTAUpdates from '../components/CTAUpdates';
+import FAQSection from '../components/FAQSection';
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <HeroSection />
       <FeatureSection />
       <AboutSection />
+      <FAQSection />
       <CTAUpdates />
     </Layout>
   );
